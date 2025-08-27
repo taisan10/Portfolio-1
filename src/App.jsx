0a02821428acbd8908c39ec97d7d376d33412fad
taisan10 <tarunbansalsgnr@gmail.com>
2025-08-27 15:17:49 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./Components/Header";
-import Hero from "../components/Hero";
+import Hero from "../components/hero.jsx";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
